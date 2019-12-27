d03aeb0168d204fb9a148cf90a5be6e922821cc6
Your Name <you@example.com>
2019-12-27 16:57:26 +0400

    Updates

---
+++ precache-manifest.e821a20a40bdfef07e9b887f6701c460.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609fa4550cf2f0e1cb30f8bc5cf46b87",
+    "url": "/rooboovi/index.html"
+  },
+  {
+    "revision": "9d566b93e5a23d62c532",
+    "url": "/rooboovi/static/css/main.9a54eb0e.chunk.css"
+  },
+  {
+    "revision": "22a6e9f3242e01fe8a3e",
+    "url": "/rooboovi/static/js/2.2e7b6fd6.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/rooboovi/static/js/2.2e7b6fd6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d566b93e5a23d62c532",
+    "url": "/rooboovi/static/js/main.6ad3acd8.chunk.js"
+  },
+  {
+    "revision": "0b816818b30447a3b045",
+    "url": "/rooboovi/static/js/runtime-main.8e8ad85f.js"
+  },
+  {
+    "revision": "6f8b4622bf25a546f2a2ff694e118726",
+    "url": "/rooboovi/static/media/Delete.6f8b4622.svg"
+  },
+  {
+    "revision": "c12b35936755ec55369b947f91e33397",
+    "url": "/rooboovi/static/media/Edit.c12b3593.svg"
+  }
+]);
